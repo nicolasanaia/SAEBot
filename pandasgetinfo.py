b9df187eef8c4024ba854a4d4fa4259986f46d16
nicolasanaia <nicolasanaia@gmail.com>
2021-03-17 22:56:46 -0300

    tracing data from spreadsheet test

--- pandasgetinfo.py
+++ pandasgetinfo.py
@@ -1,1 +1,8 @@
 import pandas as pd 
+
+df = pd.read_excel(r"/home/nick/Desktop/Developing/Projects/SAEBot/Assets/SpreadSheet/CONTROLE ANAC SAE. RVB 2021 .xlsx")  # r = reads path
+
+print(df ['ANAC'] [616])
+
+df.info()  # lista as colunas
+df2 = df.dropna('DATA PRATICO')
